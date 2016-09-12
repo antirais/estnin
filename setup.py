67a1dd842584660eef21b0713084404bc853094e
Anti Räis <antirais@gmail.com>
2016-09-12 21:35:00 +0300

    fix broken setup config

--- setup.py
+++ setup.py
@@ -18,36 +18,42 @@ VERSION = read_file('VERSION')
 
 class CleanCommand(Command):
 	"""Custom clean command to tidy up the project root."""
+
 	user_options = []
+
 	def initialize_options(self):
 		pass
+
 	def finalize_options(self):
 		pass
+
 	def run(self):
-		os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+		os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info ./eggs')
 
 setup(
 	name					= 'estnin',
 	version					= VERSION,
-	packages				= find_packages(),
+	url						= 'https://github.com/antirais/estnin',
+	py_modules				= ['estnin'],
 	license					= 'MIT',
 	include_package_data	= True,
-	# zip_safe				= False,
+	zip_safe				= True,
 	author					= 'Anti Räis',
 	author_email			= 'antirais@gmail.com',
-	description				= 'library for Estonian national identity numbers',
+	description				= 'library for handling Estonian national identity numbers',
 	long_description		= README,
 	setup_requires			= ['pytest-runner'],
 	tests_require			= ['pytest'],
 	test_suite				= "tests",
 	classifiers				= [
 		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Topic :: Software Development :: Build Tools',
 		'License :: OSI Approved :: MIT License',
-		'Natural Language :: English',
-		'Programming Language :: Python :: 3.0',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
 	],
-	install_requires = [],
-	package_data = {},
 	cmdclass = {
 		'clean': CleanCommand,
 	},
